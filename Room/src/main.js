e8509a31aecadcddd22e0afbff12d77ee8a3c4c6
Sumit Roy <147255172+sum1t7@users.noreply.github.com>
2025-07-19 23:26:25 +0530

    pianokeys

--- Room/src/main.js
+++ Room/src/main.js
@@ -4,7 +4,7 @@ import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import gsap from "gsap";
- 
+
 const clock = new THREE.Clock();
 const canvas = document.querySelector("#experience-canvas");
 const size = {
@@ -64,8 +64,6 @@ const GlassMaterial = new THREE.MeshPhysicalMaterial({
   depthWrite: false,
 });
 
-
-
 //VARIABLES
 const zAxisFans = [];
 const yAxisFans = [];
@@ -75,7 +73,7 @@ let currentIntersect = [];
 let touchHappend = false;
 let lastHovered = null;
 let plank1 = null;
-let plank2  = null;
+let plank2 = null;
 const about = [];
 const contact = [];
 const project = [];
@@ -91,9 +89,6 @@ const models = {
   contact: document.querySelector(".modal.contact"),
 };
 
-
-
-
 //Screen Video&Photo
 const videoElement = document.createElement("video");
 videoElement.src = "/textures/video/Back.mp4";
@@ -103,7 +98,7 @@ videoElement.autoplay = true;
 videoElement.play();
 
 const videoTexture = new THREE.VideoTexture(videoElement);
-videoTexture.center.set(0.5, 0.5);  
+videoTexture.center.set(0.5, 0.5);
 videoTexture.rotation = -Math.PI / 2;
 
 videoTexture.colorSpace = THREE.SRGBColorSpace;
@@ -114,10 +109,8 @@ imageElement.crossOrigin = "anonymous";
 const imageTexture = new THREE.Texture(imageElement);
 imageTexture.needsUpdate = true;
 imageTexture.offset.set(0.1, 0);
-imageTexture.repeat.set(1.2,1.2);
+imageTexture.repeat.set(1.2, 1.2);
 imageTexture.colorSpace = THREE.SRGBColorSpace;
-
- 
 
 //EventListeners
 document.querySelectorAll(".modal-exit").forEach((Button) => {
@@ -186,48 +179,78 @@ window.addEventListener("click", (event) => {
     }
   }
 });
+window.addEventListener("touchend", (e) => {
+  if (currentIntersect.length > 0) {
+    const obj = currentIntersect[0].object;
+    if (obj.name.includes("PianoKey")) {
+      const keyIndex = PianoKeys.indexOf(obj);
+      if (keyIndex !== -1) {
+        gsap.to(obj.rotation, {
+          z: 0.09,
+          duration: 0.2,
+          ease: "power2.inOut",
+          onComplete: () => {
+            gsap.to(obj.rotation, {
+              z: 0,
+              duration: 0.2,
+              ease: "power2.inOut",
+            });
+          },
+        });
+      }
+      const audio = new Audio(`/public/PianoKeys/${keyIndex}.mp3`);
+      console.log(keyIndex);
+      audio.play();
+    }
+  }
+});
 window.addEventListener("click", (e) => {
   if (currentIntersect.length > 0) {
     const obj = currentIntersect[0].object;
-    if( obj.name.includes("PianoKey")) {
-         const keyIndex = PianoKeys.indexOf(obj);
-         if (keyIndex !== -1) {
-           gsap.to(obj.rotation, {
-             
-             z: 0.09,
-             duration: 0.2,
-             ease: "power2.inOut",
-             onComplete: () => {
-               gsap.to(obj.rotation, {
-                 
-                 z: 0,
-                 duration: 0.2,
-                 ease: "power2.inOut"
-               });
-             }
-           });
-         }
-        //  const audio = new Audio(`/sounds/piano/${keyIndex + 1}.mp3`);
-        //  audio.play();  
-        
-       }
+    if (obj.name.includes("PianoKey")) {
+      const keyIndex = PianoKeys.indexOf(obj);
+      if (keyIndex !== -1) {
+        gsap.to(obj.rotation, {
+          z: 0.09,
+          duration: 0.2,
+          ease: "power2.inOut",
+          onComplete: () => {
+            gsap.to(obj.rotation, {
+              z: 0,
+              duration: 0.2,
+              ease: "power2.inOut",
+            });
+          },
+        });
       }
-  
-})
+      const audio = new Audio(`/public/PianoKeys/${keyIndex}.mp3`);
+      console.log(keyIndex);
+      audio.play();
+    }
+  }
+});
 window.addEventListener("click", () => {
   if (currentIntersect.length > 0) {
     const obj = currentIntersect[0].object;
-    if( obj.name.includes("Fish") || obj.name.includes("Blob")) {
-         animateCameraTo(obj.position);
-       }
-      }
+    if (obj.name.includes("Fish") || obj.name.includes("Blob")) {
+      animateCameraTo(obj.position);
+    }
+  }
+});
+window.addEventListener("touchend", () => {
+  if (currentIntersect.length > 0) {
+    const obj = currentIntersect[0].object;
+    if (obj.name.includes("Fish") || obj.name.includes("Blob")) {
+      animateCameraTo(obj.position);
+    }
+  }
 });
 window.addEventListener("mousemove", (event) => {
   touchHappend = false;
   pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
   pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
 });
- 
+
 window.addEventListener("resize", () => {
   size.width = window.innerWidth;
   size.height = window.innerHeight;
@@ -240,13 +263,6 @@ window.addEventListener("resize", () => {
   renderer.setSize(size.width, size.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
-
- 
-
-
-
-
-
 
 // MODEL SCENE CAMERA
 loader.load("/model/CuteProject12.glb", (glb) => {
@@ -262,14 +278,13 @@ loader.load("/model/CuteProject12.glb", (glb) => {
         });
       } else if (child.name.includes("Photo")) {
         child.material = new THREE.MeshBasicMaterial({
-         map: imageTexture
+          map: imageTexture,
         });
-      }else if (child.name.includes("Screen")) {
+      } else if (child.name.includes("Screen")) {
         child.material = new THREE.MeshBasicMaterial({
-         map: videoTexture
+          map: videoTexture,
         });
-      }
-       else {
+      } else {
         Object.keys(textureMap).forEach((key) => {
           if (child.name.includes(key)) {
             const material = new THREE.MeshBasicMaterial({
@@ -277,7 +292,7 @@ loader.load("/model/CuteProject12.glb", (glb) => {
             });
             child.material = material;
           }
-        
+
           if (
             child.name.includes("Fans30") ||
             child.name.includes("Fans23") ||
@@ -298,30 +313,27 @@ loader.load("/model/CuteProject12.glb", (glb) => {
           if (child.name.includes("Raycast")) {
             raycasterobjects.push(child);
           }
-          if(child.name.includes("PianoKey")) {
+          if (child.name.includes("PianoKey")) {
             PianoKeys.push(child);
           }
-          if( child.name.includes("HorizontalPlank")) {
-            plank1 = child
+          if (child.name.includes("HorizontalPlank")) {
+            plank1 = child;
           }
-          if( child.name.includes("VerticalPlank")) {
-            plank2 = child
+          if (child.name.includes("VerticalPlank")) {
+            plank2 = child;
           }
-          if( child.name.includes("About")) {
+          if (child.name.includes("About")) {
             about.push(child);
           }
-          if( child.name.includes("Contact")) {
+          if (child.name.includes("Contact")) {
             contact.push(child);
           }
-          if( child.name.includes("Project")) {
+          if (child.name.includes("Project")) {
             project.push(child);
           }
-          if(child.name.includes("Hover"))
-          {
+          if (child.name.includes("Hover")) {
             HoverObjects.push(child);
           }
-
-          
         });
       }
     }
@@ -360,54 +372,51 @@ controls.maxAzimuthAngle = THREE.MathUtils.degToRad(-10);
 controls.minDistance = 5;
 controls.maxDistance = 80;
 controls.enablePan = false;
- 
 
 let originalCamPos = camera.position.clone();
-let originalTarget = controls.target.clone();  
-
-
+let originalTarget = controls.target.clone();
 
 const showModal = (modal) => {
   modal.style.display = "block";
   const tl = gsap.timeline();
   tl.set(modal, { opacity: 1, scale: 0 })
-    .to(modal, { opacity: 1, scale: 1.25, duration: 0.2 , ease: "power2.inOut"})
-    .to(modal, { scale: 0.95, duration: 0.2 , ease: "power2.inOut"})
+    .to(modal, { opacity: 1, scale: 1.25, duration: 0.2, ease: "power2.inOut" })
+    .to(modal, { scale: 0.95, duration: 0.2, ease: "power2.inOut" })
     .to(modal, { opacity: 1, scale: 1, duration: 0.2, ease: "power2.inOut" });
 };
 const hideModal = (modal) => {
   const tl = gsap.timeline();
   tl.set(modal, { opacity: 1, scale: 1 })
-    .to(modal, { opacity: 1, scale: 1.25, duration: 0.2 , ease: "power2.inOut"})
+    .to(modal, { opacity: 1, scale: 1.25, duration: 0.2, ease: "power2.inOut" })
     .to(modal, { opacity: 0, scale: 0, duration: 0.2, ease: "power2.inOut" });
 };
 function animateCameraTo(objectPosition) {
-  const newCamPos = objectPosition.clone().add(new THREE.Vector3(2, 0, 2));  
+  const newCamPos = objectPosition.clone().add(new THREE.Vector3(2, 0, 2));
 
-   gsap.to(camera.position, {
+  gsap.to(camera.position, {
     x: newCamPos.x,
     y: newCamPos.y,
     z: newCamPos.z,
     duration: 0.6,
-    ease: "power2.inOut"
+    ease: "power2.inOut",
   });
 
-   gsap.to(controls.target, {
+  gsap.to(controls.target, {
     x: objectPosition.x,
     y: objectPosition.y,
     z: objectPosition.z,
     duration: 0.3,
     ease: "power2.inOut",
-    onUpdate: () => controls.update()
+    onUpdate: () => controls.update(),
   });
 
-   setTimeout(() => {
+  setTimeout(() => {
     gsap.to(camera.position, {
       x: originalCamPos.x,
       y: originalCamPos.y,
       z: originalCamPos.z,
       duration: 1.5,
-      ease: "power2.inOut"
+      ease: "power2.inOut",
     });
 
     gsap.to(controls.target, {
@@ -416,31 +425,28 @@ function animateCameraTo(objectPosition) {
       z: originalTarget.z,
       duration: 1.5,
       ease: "power2.inOut",
-      onUpdate: () => controls.update()
+      onUpdate: () => controls.update(),
     });
   }, 2000);
-};
+}
 function hoverEffect(obj) {
   gsap.to(obj.scale, {
     x: 1.2,
     y: 1.2,
     z: 1.2,
     duration: 0.4,
-    ease: "power2.out"
+    ease: "power2.out",
   });
-
-};
+}
 function resetObject(obj) {
   gsap.to(obj.scale, {
     x: 1,
     y: 1,
     z: 1,
     duration: 0.3,
-    ease: "power2.inOut"
+    ease: "power2.inOut",
   });
-
-  
-};
+}
 function handleRaycasterInteraction() {
   if (currentIntersect.length > 0) {
     const currentIntersectObject = currentIntersect[0].object;
@@ -463,12 +469,11 @@ function handleRaycasterInteraction() {
       showModal(models.contact);
     }
   }
-};
-function introAnimation(){
-  const t1 = gsap.timeline({ defaults: { duration: 1, ease: "power2.inOut" } });
-
 }
- 
+function introAnimation() {
+  const t1 = gsap.timeline({ defaults: { duration: 1, ease: "power2.inOut" } });
+}
+
 function animate() {
   raycaster.setFromCamera(pointer, camera);
   const intersects = raycaster.intersectObjects(HoverObjects, true); // raycast only against hoverable objects
@@ -476,7 +481,7 @@ function animate() {
   if (intersects.length > 0) {
     const obj = intersects[0].object;
     const keyIndex = HoverObjects.indexOf(obj);
-    
+
     if (keyIndex !== -1) {
       if (lastHovered !== keyIndex) {
         if (lastHovered !== null && HoverObjects[lastHovered]) {
@@ -499,8 +504,6 @@ function animate() {
 
 animate();
 
- 
- 
 const render = () => {
   zAxisFans.forEach((fan) => {
     fan.rotation.z += 0.01;
@@ -511,11 +514,9 @@ const render = () => {
   });
   chairTop.forEach((chair) => {
     chair.rotation.y += Math.sin(clock.getElapsedTime()) * 0.0004;
-
   });
-   
 
-   //Raycasting
+  //Raycasting
   raycaster.setFromCamera(pointer, camera);
   currentIntersect = raycaster.intersectObjects(raycasterobjects);
   for (let i = 0; i < currentIntersect.length; i++) {}
