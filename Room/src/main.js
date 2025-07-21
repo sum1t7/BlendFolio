d6f1ffed4586b561c7d7a2b2cccc7ad0d5ac4235
Sumit Roy <147255172+sum1t7@users.noreply.github.com>
2025-07-22 00:19:23 +0530

    loading,audio,theme, almost done

--- Room/src/main.js
+++ Room/src/main.js
@@ -4,6 +4,11 @@ import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import gsap from "gsap";
+import backgroundMusicSrc from "/Audios/BackgroungMusic.mp3";
+
+const backgroundMusic = new Audio(backgroundMusicSrc);
+backgroundMusic.loop = true;
+backgroundMusic.volume = 0.5;
 
 const clock = new THREE.Clock();
 const canvas = document.querySelector("#experience-canvas");
@@ -13,13 +18,16 @@ const size = {
 };
 
 //Loaders
-const textureLoader = new THREE.TextureLoader();
+const manager = new THREE.LoadingManager();
+const loadingScreen = document.querySelector(".loading-screen");
+const loadingScreenButton = document.querySelector(".loading-button");
+const textureLoader = new THREE.TextureLoader(manager);
 const dracoLoader = new DRACOLoader();
 dracoLoader.setDecoderPath("/draco/");
-const environmentMap = new THREE.CubeTextureLoader()
+const environmentMap = new THREE.CubeTextureLoader(manager)
   .setPath("/textures/envmap/")
   .load(["px.webp", "nx.webp", "py.webp", "ny.webp", "pz.webp", "nz.webp"]);
-const loader = new GLTFLoader();
+const loader = new GLTFLoader(manager);
 loader.setDRACOLoader(dracoLoader);
 const raycaster = new THREE.Raycaster();
 const pointer = new THREE.Vector2();
@@ -71,12 +79,10 @@ const chairTop = [];
 const raycasterobjects = [];
 let currentIntersect = [];
 let touchHappend = false;
-let lastHovered = null;
-let plank1 = null;
-let plank2 = null;
-const about = [];
-const contact = [];
-const project = [];
+let about;
+let contact;
+let project;
+const pointers = [];
 const PianoKeys = [];
 let currentHoverObject = null;
 const socialLinks = {
@@ -110,8 +116,6 @@ imageTexture.needsUpdate = true;
 imageTexture.offset.set(0.1, 0);
 imageTexture.repeat.set(1.2, 1.2);
 imageTexture.colorSpace = THREE.SRGBColorSpace;
-
-
 
 //EventListeners
 document.querySelectorAll(".modal-exit").forEach((Button) => {
@@ -200,9 +204,12 @@ window.addEventListener("touchend", (e) => {
           },
         });
       }
-      const audio = new Audio(`PianoKeys/${keyIndex}.mp3`);
+      const audio = new Audio(`/Audios/${keyIndex}.mp3`);
       console.log(keyIndex);
-      audio.play();
+      audio.play().catch((err) => {
+        // Handle autoplay restrictions or playback errors
+        console.warn("Audio playback failed:", err);
+      });
     }
   }
 });
@@ -225,16 +232,22 @@ window.addEventListener("click", (e) => {
           },
         });
       }
-      const audio = new Audio(`/PianoKeys/${keyIndex}.mp3`);
-      console.log(keyIndex);
+      const audio = new Audio(`/Audios/${keyIndex}.mp3`);
+      backgroundMusic.pause();
       audio.play();
+      setTimeout(() => {
+        audio.addEventListener("ended", () => {
+        backgroundMusic.play();
+      });
+      }, 1000);
+       
     }
   }
 });
 window.addEventListener("click", () => {
   if (currentIntersect.length > 0) {
     const obj = currentIntersect[0].object;
-    if (obj.name.includes("Fish") || obj.name.includes("Blob")) {
+    if (obj.name.includes("Fish")) {
       animateCameraTo(obj.position);
     }
   }
@@ -268,13 +281,13 @@ window.addEventListener("resize", () => {
 });
 
 // MODEL SCENE CAMERA
-loader.load("/model/CuteProject12.glb", (glb) => {
+loader.load("/model/CuteProject13-v1.glb", (glb) => {
   glb.scene.traverse((child) => {
     if (child.isMesh) {
-       if (child.name.includes("Hover")) {
-            child.userData.initialScale = new THREE.Vector3().copy(child.scale);
-            child.userData.isAnimating = false;
-          }
+      if (child.name.includes("Hover")) {
+        child.userData.initialScale = new THREE.Vector3().copy(child.scale);
+        child.userData.isAnimating = false;
+      }
       if (child.name.includes("Glass")) {
         child.material = GlassMaterial;
       } else if (child.name.includes("Water")) {
@@ -324,25 +337,34 @@ loader.load("/model/CuteProject12.glb", (glb) => {
             PianoKeys.push(child);
           }
           if (child.name.includes("HorizontalPlank")) {
-            plank1 = child;
+            pointers.push(child);
+            child.scale.set(0, 0, 0);
           }
           if (child.name.includes("VerticalPlank")) {
-            plank2 = child;
+            pointers.push(child);
+            child.scale.set(0, 0, 0);
           }
           if (child.name.includes("About")) {
-            about.push(child);
+            about = child;
+            about.scale.set(0, 0, 0);
           }
           if (child.name.includes("Contact")) {
-            contact.push(child);
+            contact = child;
+            contact.scale.set(0, 0, 0);
           }
           if (child.name.includes("Project")) {
-            project.push(child);
+            project = child;
+            project.scale.set(0, 0, 0);
           }
-          
+          if (child.name.includes("Pointer")) {
+            pointers.push(child);
+            child.scale.set(0, 0, 0);
+          }
         });
       }
     }
   });
+
   scene.add(glb.scene);
 });
 const scene = new THREE.Scene();
@@ -375,7 +397,7 @@ controls.maxPolarAngle = THREE.MathUtils.degToRad(80);
 controls.minAzimuthAngle = THREE.MathUtils.degToRad(-70);
 controls.maxAzimuthAngle = THREE.MathUtils.degToRad(-10);
 controls.minDistance = 5;
-controls.maxDistance = 80;
+controls.maxDistance = 90;
 controls.enablePan = false;
 
 let originalCamPos = camera.position.clone();
@@ -383,6 +405,9 @@ let originalTarget = controls.target.clone();
 
 const showModal = (modal) => {
   modal.style.display = "block";
+  const click = new Audio("/Audios/Click.mp3");
+  click.currentTime = 0;
+  click.play();
   const tl = gsap.timeline();
   tl.set(modal, { opacity: 1, scale: 0 })
     .to(modal, { opacity: 1, scale: 1.25, duration: 0.3, ease: "power2.inOut" })
@@ -390,6 +415,9 @@ const showModal = (modal) => {
     .to(modal, { opacity: 1, scale: 1, duration: 0.3, ease: "power2.inOut" });
 };
 const hideModal = (modal) => {
+  const close = new Audio("/Audios/Close.wav");
+  close.currentTime = 0;
+  close.play();
   const tl = gsap.timeline();
   tl.set(modal, { opacity: 1, scale: 1 })
     .to(modal, { opacity: 1, scale: 1.25, duration: 0.2, ease: "power2.inOut" })
@@ -398,9 +426,11 @@ const hideModal = (modal) => {
 
 function animateCameraTo(objectPosition) {
   const newCamPos = objectPosition.clone().add(new THREE.Vector3(2, 0, 2));
-  const audio = new Audio("/PianoKeys/FishMusic.mp3");
+  const audio = new Audio("/Audios/FishMusic.mp3");
   audio.currentTime = 0;
+  backgroundMusic.pause();
   audio.play();
+  
   gsap.to(camera.position, {
     x: newCamPos.x,
     y: newCamPos.y,
@@ -435,6 +465,7 @@ function animateCameraTo(objectPosition) {
       ease: "power2.inOut",
       onUpdate: () => controls.update(),
     });
+    backgroundMusic.play();
   }, 4000);
 }
 
@@ -474,7 +505,6 @@ function playHoverAnimation(object, isHovering) {
       duration: 0.3,
       ease: "back.in(1.8)",
     });
- 
   } else {
     gsap.to(object.scale, {
       x: object.userData.initialScale.x,
@@ -486,11 +516,80 @@ function playHoverAnimation(object, isHovering) {
   }
 }
 
-function playInroAnimation(){
-
+function playintroanimation(object) {
+  gsap.to(object.scale, {
+    x: 1,
+    y: 1,
+    z: 1,
+    duration: 1,
+    ease: "back.out(1.8)",
+  });
 }
 
+manager.onLoad = function () {
+  loadingScreenButton.style.border = "8px solid #2a0f4e";
+  loadingScreenButton.style.background = "#401d49";
+  loadingScreenButton.style.color = "#e6dede";
+  loadingScreenButton.style.boxShadow = "rgba(0, 0, 0, 0.24) 0px 3px 8px";
+  loadingScreenButton.textContent = "Enter!";
+  loadingScreenButton.style.cursor = "pointer";
+  loadingScreenButton.style.transition =
+    "transform 0.4s cubic-bezier(0.34, 1.56, 0.64, 1)";
+  let isDisabled = false;
 
+  function handleEnter() {
+    if (isDisabled) return;
+
+    loadingScreenButton.style.cursor = "default";
+    loadingScreenButton.style.border = "8px solid #6e5e9c";
+    loadingScreenButton.style.background = "#ead7ef";
+    loadingScreenButton.style.color = "#6e5e9c";
+    loadingScreenButton.style.boxShadow = "none";
+    loadingScreen.style.background = "#ead7ef";
+
+    const tl = gsap.timeline();
+    tl.to(loadingScreen, {
+      scale: 0.7,
+      rotateZ: -3,
+      duration: 0.4,
+      ease: "back.out(1.5)",
+    }).to(loadingScreen, {
+      y: 1000,
+      opacity: 1,
+      duration: 0.4,
+      ease: "power2.inOut",
+      onComplete: () => {
+        backgroundMusic.play();
+        loadingScreen.style.display = "none";
+        loadingScreen.style.pointerEvents = "none";
+        pointers.forEach((pointer) => {
+          playintroanimation(pointer);
+        });
+      },
+    });
+
+    isDisabled = true;
+  }
+
+  loadingScreenButton.addEventListener("mouseenter", () => {
+    loadingScreenButton.style.transform = "scale(1.3)";
+  });
+
+  loadingScreenButton.addEventListener("touchend", (e) => {
+    touchHappend = true;
+    e.preventDefault();
+    handleEnter();
+  });
+
+  loadingScreenButton.addEventListener("click", (e) => {
+    if (touchHappend) return;
+    handleEnter();
+  });
+
+  loadingScreenButton.addEventListener("mouseleave", () => {
+    loadingScreenButton.style.transform = "none";
+  });
+};
 
 const render = () => {
   zAxisFans.forEach((fan) => {
@@ -511,8 +610,8 @@ const render = () => {
   if (currentIntersect.length > 0) {
     const currentIntersectObject = currentIntersect[0].object;
 
-    if (currentIntersectObject.name.includes("Hover") ) {
-      if (currentHoverObject !== currentIntersectObject ) {
+    if (currentIntersectObject.name.includes("Hover")) {
+      if (currentHoverObject !== currentIntersectObject) {
         if (currentHoverObject) {
           playHoverAnimation(currentHoverObject, false);
         }
@@ -520,7 +619,6 @@ const render = () => {
         currentHoverObject = currentIntersectObject;
       }
     }
- 
 
     if (currentIntersectObject.name.includes("Pointer")) {
       document.body.style.cursor = "pointer";
